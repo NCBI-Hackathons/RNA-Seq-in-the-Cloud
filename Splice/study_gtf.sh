fb89028e216f5f21b52c5d40ce322510c0b478d0
Vadim Zalunin <zaluninvv@ncbi.nlm.nih.gov>
2019-03-12 13:37:58 +0000

    changed bam naming

--- Splice/study_gtf.sh
+++ Splice/study_gtf.sh
@@ -11,11 +11,10 @@ while read line; do
   gsutil -m cp -P -n $(for run in $(echo "$runs" | tr , ' '); do
     echo -ne " gs://ncbi_sra_rnaseq/$run.bam"
   done) . 
-  tmpbam=$(mktemp merged.XXX.bam)
+  tmpbam="${study}_${tissue}.merged.bam"
   samtools merge -@4 -f $tmpbam *.bam
-  tmpbam='merged.84a.bam'
   samtools index $tmpbam
-  chrbam=$(mktemp chr.XXX.bam)
+  chrbam="${study}_${tissue}.bam"
   samtools view -h $tmpbam chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY chrM | samtools view -@$threads -Sb - > $chrbam
   gtfout="${study}_${tissue}.gtf"
   stringtie $chrbam -p $threads -G $gtfin -o $gtfout
