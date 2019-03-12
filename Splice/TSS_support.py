3edf1a94731514ec08f55c9b8845be708f7ac2da
Maggie Cai <33357176+maggiecdagger@users.noreply.github.com>
2019-03-12 13:00:34 -0400

    Update TSS_support.py

--- Splice/TSS_support.py
+++ Splice/TSS_support.py
@@ -1,36 +1,33 @@
 import sys
 import csv
+from collections import defaultdict
 
 novel_file=sys.argv[1]
 TSS_file=sys.argv[2]
-
-tss_list=list()
-
-#mix_list=list()
-the_file=open('novel_tss.tsv', 'w')
+tss_list=dict()
+novel_list=dict()
+the_file=open('test_tight_novel_%s.tsv'%novel_file, 'w')
+dist=10
 with open(TSS_file , 'r') as tss_f:
     for line in tss_f:
         fields = line.split('\t')
-        tss_list.append([fields[0],fields[2],fields[3],fields[4],fields[8]])
-
+        fields2=fields[8].split(';')
+        if fields[2]=='region':
+            tss_list[(fields[0],fields[6],fields2[2])]=(fields[3],fields[4])
+            #chr, strand, id:start,stop
+print(tss_list.items())
 with open(novel_file,'r') as novel_f:
     for line in novel_f:
         fields = line.split('\t')
-        for fields1 in tss_list:
-            if fields1[1]=='transcription_start_site':
-                if fields[0] == fields1[0]:
-                    with open('novel_tss.tsv', 'a') as the_file:
-                        wr= csv.writer(the_file,delimiter='\t',escapechar=' ', quoting=csv.QUOTE_NONE)
-                        if fields[1]>=fields1[2]:
-                            if fields[2]<=fields1[3]:
-#                                mix_list.append([fields[0],fields[1],fields[2],fields1[2],fields1[3],fields1[4],'TSS inside'])
-                                wr.writerow([fields[0],fields[1],fields[2],fields1[2],fields1[3],fields1[4],'TSS inside'])
-                        elif fields[2]>=fields1[2]:
-#                            mix_list.append([fields[0],fields[1],fields[2],fields1[2],fields1[3],fields1[4],'before the TSS'])
-                            wr.writerow([fields[0],fields[1],fields[2],fields1[2],fields1[3],fields1[4],'before the TSS'])
-                        elif fields[1]<=fields1[3]:
-#                            mix_list.append([fields[0],fields[1],fields[2],fields1[2],fields1[3],fields1[4],'after the TSS'])
-                            wr.writerow([fields[0],fields[1],fields[2],fields1[2],fields1[3],fields1[4],'after the TSS'])
-
-                                
-
+        if fields[3].strip()=='-':
+            novel_list[(fields[0],fields[3],fields[4])]=fields[2]
+        if fields[3].strip()=='+':
+            novel_list[(fields[0],fields[3],fields[4])]=fields[1]
+#chr, strand, id:start
+the_file=open('test_tight_novel_%s.tsv'%novel_file, 'a') 
+wr= csv.writer(the_file,delimiter='\t',escapechar=' ', quoting=csv.QUOTE_NONE)
+for key_t,loc_t in tss_list.items():
+    for key_n,loc_n in novel_list.items():
+        if key_t[0]==key_n[0] and key_t[1].strip()==key_n[1].strip():
+            if int(loc_n)>=(int(loc_t[0])-dist) and int(loc_n)<=(int(loc_t[1])-dist):
+                wr.writerow([key_n[0],key_n[1],key_n[2],key_t[1],key_t[2]])
