7932a764ef8ec98cba555b16a6e452f7cfd93d2f
Ariella Gladstein <agladstein@email.arizona.edu>
2019-03-12 13:43:14 -0400

    metadata refactor function file

--- Metadata/query_runs.py
+++ Metadata/query_runs.py
@@ -1,20 +1,7 @@
 import sys
 import json
 
-
-def term_to_run(data_dict, term):
-
-    runs_with_term = []
-    runs_without_term = []
-    for samples, terms in data_dict.items():
-        if term in terms:
-            runs_with_term.append(samples)
-        else:
-            runs_without_term.append(samples)
-
-    assert set(runs_with_term) != set(runs_without_term), 'Oops! The set of samples with the term and without the term overlap!'
-
-    return runs_with_term, runs_without_term
+from functions import *
 
 
 def main():
