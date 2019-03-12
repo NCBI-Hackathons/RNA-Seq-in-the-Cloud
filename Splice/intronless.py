d019a5aa5e9cc039858c17a65f1a1e7f02d662f3
Maggie Cai <33357176+maggiecdagger@users.noreply.github.com>
2019-03-12 13:00:56 -0400

    Update intronless.py

--- Splice/intronless.py
+++ Splice/intronless.py
@@ -5,33 +5,21 @@ import csv
 #import pandas as pd
 
 gtffile=sys.argv[1]
-#fh = open(gtffile, 'r').readlines()
+f=open(gtffile , 'r')
 
-the_file=open('novel.tsv', 'w')
+the_file=open('novel_%s.tsv'%gtffile, 'w')
 
-mixlist=list()
-with open(gtffile , 'r') as fh:
-    next(fh)
-    next(fh)
-    (fh)
-    for line in fh:
-        fields = line.split('\t')
+fh=f.readlines()[2:(-1)]
+i=1
+print(len(fh))
+for i in range (2,(len(fh)-2)):
+    with open('novel_%s.tsv'%gtffile, 'a') as the_file:
+        wr= csv.writer(the_file,delimiter='\t',escapechar=' ', quoting=csv.QUOTE_NONE)
+        fields = fh[i].split('\t')
+        ref=fields[8].split(';')[3].strip()
+        if not ref.startswith("ref"):
+            fields3 = fh[i+2].split('\t')
+            if fields[2] == 'transcript' and fields3[2] == 'exon':
+                line = [fields[0],fields[3],fields[4],fields[6],fields[8].split(';')[1]]
+                wr.writerow(line)
 
-        line2 = fh.next()
-        line3 = fh.next()
-        fields3 = line3.split('\t')
-        if fields[2]=='transcript' and fields3[2]=='transcript':
-            
-            cub_this = [fields[0],fields[3],fields[4],fields[6]]
-            cub_next = [fields3[0],fields3[3],fields3[4],fields3[6]]
-            if cub_this not in mixlist:
-                mixlist.append(cub_this)
-                
-            if cub_next not in mixlist:
-                mixlist.append(cub_next)
-print(mixlist)                
-with open('novel.tsv', 'a') as the_file:
-#                print(type(fields3))
-    wr= csv.writer(the_file,delimiter='\t',escapechar=' ', quoting=csv.QUOTE_NONE)                 
-    for line in mixlist:
-        wr.writerow(line)
