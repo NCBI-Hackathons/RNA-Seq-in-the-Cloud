b46b6344f4dff74996b9e9e3430980540bed306f
Khun Zaw Latt <khunzawlatt@gmail.com>
2019-03-13 13:07:04 -0400

    plots and table for Metadata

--- Metadata/Metadata_plot.R
+++ Metadata/Metadata_plot.R
@@ -8,13 +8,20 @@ query_disease_metadata <- subset(hack_data, V1 %in% query_disease_samples)
 query_disease_metadata_table <- as.data.frame(table(query_disease_metadata$V2)) 
 
 query_disease_metadata_sorted <- query_disease_metadata_table[order(query_disease_metadata_table$Freq, decreasing = TRUE),]
+query_disease_metadata_sorted$Metadata <- query_disease_metadata_sorted$Var1
 
-query_disease_metadata_sorted$percentage <- query_disease_metadata_sorted$Freq/query_disease_metadata_sorted[1,2]
-query_disease_metadata_top10 <- query_disease_metadata_sorted[which(query_disease_metadata_sorted$percentage >= 0.1),]
+query_disease_metadata_sorted$Proportion <- query_disease_metadata_sorted$Freq/query_disease_metadata_sorted[1,2]
+query_disease_metadata_top10 <- query_disease_metadata_sorted[which(query_disease_metadata_sorted$Proportion >= 0.1),]
 
-query_disease_metadata_top10$Metadata <- query_disease_metadata_top10$Var1
+
 
 library(ggplot2)
 
-p <-ggplot(query_disease_metadata_top10, aes(Metadata, percentage)) + geom_bar(stat = "identity") +
+## Bar graph for proportion of top metadata
+bp <- ggplot(query_disease_metadata_top10, aes(x = reorder(Metadata, -Proportion), y = Proportion)) +
+  xlab("Metadata") + 
+  ggtitle("Metadata available in 10 percent or more of samples") +
+  geom_bar(stat = "identity") +
   theme(axis.text.x = element_text(angle = 60, hjust = 1))
+
+bp
