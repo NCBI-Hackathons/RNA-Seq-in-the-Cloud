723b8010aade54247518bc1c500c0e9b70e75c59
xu-hao <xuh@cs.unc.edu>
2019-04-08 21:06:19 -0400

    Create normlize.py

---
+++ Generative Adversarial Networks/src/normlize.py
@@ -0,0 +1,15 @@
+import argparse
+import pandas as pd
+import numpy as np
+
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument("--input_file", type=str,required=True)
+parser.add_argument("--output_file", type=str,required=True)
+parser.add_argument("--case_control", type=int, required=True)
+args = parser.parse_args()
+case_f = pd.read_csv(args.input_file, header=None)
+mean = np.mean(case_f)
+std = np.std(case_f)
+normalize_case_df = case_f.applymap(lambda x: (x-mean)/std)
+normalize_case_df["case_control"] = args.case_control
+normalize_case_df.to_csv(args.output_file,index=False)
