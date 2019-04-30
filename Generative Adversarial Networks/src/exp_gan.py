391a513a2bc0aefbf569832e04109e871d233cc9
Hao Xu <xuhao@pascal.renci.org>
2019-04-30 14:50:07 -0400

    update gan code

--- Generative Adversarial Networks/src/exp_gan.py
+++ Generative Adversarial Networks/src/exp_gan.py
@@ -52,7 +52,6 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
 # config.gpu_options.per_process_gpu_memory_fraction=0.25
 # config.log_device_placement = True  # to log device placement (on which device the operation ran)
-# (nothing gets printed in Jupyter, only if you run it standalone)
 sess = tf.Session(config=config)
 set_session(sess)  # set this TensorFlow session as the default session for Keras
 
